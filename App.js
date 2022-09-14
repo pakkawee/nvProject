3d44a020936e802a510453b449ea227a70d400f1
pakkawee <pakkawee.1@gmail.com>
2022-09-14 10:33:01 +0700

    prac 14Sep

--- App.js
+++ App.js
@@ -1,48 +1,105 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { View, Text, Button,SafeAreaView , Image} from "react-native";
+import React from "react";
+import { NavigationContainer,DefaultTheme} from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
 
-function HomeScreen({ navigation }) {
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "rgb(255,45,85)",
+  },
+};
+
+function CustomDrawerContent(props) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home</Text>
-      <Button title="Go to Setting" onPress={() => navigation.navigate("Setting")} />
-    </View>
-  )
+    <SafeAreaView>
+      <Image
+        source={require("./assets/react_logo.png")}
+      />
+      <DrawerContentScrollView {...props}>
+        <DrawerItemList {...props} />
+      </DrawerContentScrollView>
+    </SafeAreaView>
+  );
+}
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      useLegacyImplementation
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        drawerStyle: {
+          width: 240,
+        },
+      }}
+    >
+      <Drawer.Screen name="Home" component={MyTab}/>
+      {/* <Drawer.Screen name="Feed" component={FeedScreen} /> */}
+      <Drawer.Screen name="Setting" component={MyTab} />
+    </Drawer.Navigator>
+  );
 }
 
+function HomeScreen({navigation}) {
+  return (
+    
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Ionicons name="home" size={30} color="red" />
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function SettingScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Setting!</Text>
+      <Button title="Go to Home" onPress={()=>navigation.navigate("Home")}/>
+    </View>
+  );
+}
 const Tab = createBottomTabNavigator();
 
-function MyTabs() {
+function MyTab() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator  screenOptions={({ route }) => ({
+      tabBarIcon: ({focused, color, size}) => {
+        let iconName;
+        if(route.name ==='Home') {
+          iconName = focused
+          ? 'ios-information-circle'
+          : 'ios-information-circle-outline'
+        }else if (route.name === 'Setting'){
+          iconName = focused ? 'ios-list-box' : 'ios-list'
+        }
+        return <Ionicons name={iconName} size={size} color={color}/>
+      },
+      tabBarActiveTintColor:'tomato',
+      tabBarInactiveTintColor:'gray'
+    })}>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Setting" component={SettingScreen} />
     </Tab.Navigator>
-  )
-}
-
-function SettingScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Setting</Text>
-      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
-    </View>
-  )
+  );
 }
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <MyTabs/>
+    <NavigationContainer theme={MyTheme}>
+      <MyDrawer/>
     </NavigationContainer>
+  );
+};
 
-  )
-}
-
-
-
-export default App
-
-const styles = StyleSheet.create({})
+export default App;
